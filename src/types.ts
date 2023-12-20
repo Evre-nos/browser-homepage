a566453689de9b0406d40b7479798b7114a0c809
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-12-20 12:27:22 -0500

    ts file is now loaded as a module added LinkList and LinkObject types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,15 @@
-type weatherInfo = {
+export type Temperature = {
   temp: number;
+};
+
+export type LinkList = {
+  work: [Object];
+  bonfire: [Object];
+};
+
+export type LinkObject = {
+  name: string;
+  group: string;
+  mode: string;
+  url: string;
 };
