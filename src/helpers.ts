1461955b83443ea1590a2bfbcebefcbafc3e1d86
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2024-01-12 13:53:33 -0500

    Separated switchModes() function to switchToWork() + switchToBonfire() switchModes() now calls switchToWork() and switchToBonfire() updated init() close #7

--- src/helpers.ts
+++ src/helpers.ts
@@ -15,7 +15,7 @@ export async function fetchWeather(date: Date): Promise<string> {
   return `${data.hourly.temperature_2m[date.getHours()]}`;
 }
 
-export function switchModes(data: LinkList): void {
+export function switchToWork(data: LinkList): void {
   const title =
     (document.querySelector('[data-list-title]') as HTMLLIElement) || null;
   const infoBar =
@@ -30,50 +30,78 @@ export function switchModes(data: LinkList): void {
     (document.querySelector('.right-container') as HTMLElement) || null;
   const linksContainer =
     (document.querySelector('.links-container') as HTMLElement) || null;
+  localStorage.setItem('mode', 'work');
+  rightContainer.removeChild(linksContainer);
+  pic.removeAttribute('data-picture');
+  pic.setAttribute('data-picture', 'work');
+  title.removeAttribute('data-list-title');
+  title.setAttribute('data-list-title', 'work');
+  infoBar.removeAttribute('data-info-bar');
+  infoBar.setAttribute('data-info-bar', 'work');
+  body.removeAttribute('data-body');
+  body.setAttribute('data-body', 'work');
+  directory.removeAttribute('data-directory');
+  directory.setAttribute('data-directory', 'work');
+  directory.innerHTML = '&gt; cd ~/work/<span class="blinking">_</span>';
+  rightContainer.appendChild(createWorkLinksEl(data));
+}
 
-  if (title.dataset.listTitle == 'bonfire') {
-    localStorage.setItem('mode', 'work');
-    rightContainer.removeChild(linksContainer);
-    pic.removeAttribute('data-picture');
-    pic.setAttribute('data-picture', 'work');
-    title.removeAttribute('data-list-title');
-    title.setAttribute('data-list-title', 'work');
-    infoBar.removeAttribute('data-info-bar');
-    infoBar.setAttribute('data-info-bar', 'work');
-    body.removeAttribute('data-body');
-    body.setAttribute('data-body', 'work');
-    directory.removeAttribute('data-directory');
-    directory.setAttribute('data-directory', 'work');
-    directory.innerHTML = '&gt; cd ~/work/<span class="blinking">_</span>';
-    rightContainer.appendChild(createWorkLinksEl(data));
+export function switchToBonfire(data: LinkList): void {
+  const title =
+    (document.querySelector('[data-list-title]') as HTMLLIElement) || null;
+  const infoBar =
+    (document.querySelector('[data-info-bar]') as HTMLDivElement) || null;
+  const body =
+    (document.querySelector('[data-body]') as HTMLBodyElement) || null;
+  const directory =
+    (document.querySelector('[data-directory]') as HTMLParagraphElement) ||
+    null;
+  const pic = (document.getElementById('picture') as HTMLImageElement) || null;
+  const rightContainer =
+    (document.querySelector('.right-container') as HTMLElement) || null;
+  const linksContainer =
+    (document.querySelector('.links-container') as HTMLElement) || null;
+  localStorage.setItem('mode', 'bonfire');
+  rightContainer.removeChild(linksContainer);
+  pic.removeAttribute('data-picture');
+  pic.setAttribute('data-picture', 'bonfire');
+  title.removeAttribute('data-list-title');
+  title.setAttribute('data-list-title', 'bonfire');
+  infoBar.removeAttribute('data-info-bar');
+  infoBar.setAttribute('data-info-bar', 'bonfire');
+  body.removeAttribute('data-body');
+  body.setAttribute('data-body', 'bonfire');
+  directory.removeAttribute('data-directory');
+  directory.setAttribute('data-directory', 'bonfire');
+  directory.innerHTML = '&gt; cd ~/bonfire/<span class="blinking">_</span>';
+  rightContainer.appendChild(createBonfireLinkEl(data));
+}
+
+export function switchModes(data: LinkList) {
+  const title =
+    (document.querySelector('[data-list-title]') as HTMLLIElement) || null;
+
+  if (title.dataset.listTitle == 'work') {
+    switchToBonfire(data);
   } else {
-    localStorage.setItem('mode', 'bonfire');
-    rightContainer.removeChild(linksContainer);
-    pic.removeAttribute('data-picture');
-    pic.setAttribute('data-picture', 'bonfire');
-    title.removeAttribute('data-list-title');
-    title.setAttribute('data-list-title', 'bonfire');
-    infoBar.removeAttribute('data-info-bar');
-    infoBar.setAttribute('data-info-bar', 'bonfire');
-    body.removeAttribute('data-body');
-    body.setAttribute('data-body', 'bonfire');
-    directory.removeAttribute('data-directory');
-    directory.setAttribute('data-directory', 'bonfire');
-    directory.innerHTML = '&gt; cd ~/bonfire/<span class="blinking">_</span>';
-    rightContainer.appendChild(createBonfireLinkEl(data));
+    switchToWork(data);
   }
 }
+
 export function init(data: LinkList): void {
   const rightContainer =
     (document.querySelector('.right-container') as HTMLElement) || null;
   if (localStorage.getItem('mode') == null) {
     const initStartMode = createBonfireLinkEl(data);
     rightContainer.appendChild(initStartMode);
+    switchToBonfire(data);
   } else if (localStorage.getItem('mode') == 'bonfire') {
     const initStartMode = createBonfireLinkEl(data);
     rightContainer.appendChild(initStartMode);
+    switchToBonfire(data);
   } else if (localStorage.getItem('mode') == 'work') {
     const initStartMode = createWorkLinksEl(data);
     rightContainer.appendChild(initStartMode);
+    switchToWork(data);
   }
 }
